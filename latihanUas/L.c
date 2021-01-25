30d734e03efd129d0e58a3d9ed87a1db3f35fc08
agusthas <suzumantan@gmail.com>
2021-01-26 04:27:41 +0700

    finish problem L

--- latihanUas/L.c
+++ latihanUas/L.c
@@ -26,24 +26,88 @@ int main() {
   return 0;
 }
 
+int myCompa(const void *a, const void *b) {
+  if (*(const long long int *)a < *(const long long int *)b) return -1;
+  return *(const long long int *)a > *(const long long int *)b;
+}
+
+ll leftmost(ll *arr, ll n, ll key) {
+  ll l = 0, r = n - 1, res = -1;
+  while (l <= r) {
+    ll mid = l + (r - l) / 2;
+    if (arr[mid] >= key) {
+      res = mid;
+      r = mid - 1;
+    } else if (arr[mid] < key) {
+      l = mid + 1;
+    }
+  }
+
+  return res;
+}
+
+ll rightmost(ll *arr, ll n, ll key) {
+  ll l = 0, r = n - 1, res = -1;
+  while (l <= r) {
+    ll mid = l + (r - l) / 2;
+    if (arr[mid] > key) {
+      r = mid - 1;
+    } else if (arr[mid] <= key) {
+      res = mid;
+      l = mid + 1;
+    }
+  }
+
+  return res;
+}
+
 void solve() {
-  ll N, Q;
+  ll N;
   scanf("%lld", &N);
   ll arr[N + 4];
   for (int i = 0; i < N; i++) {
     scanf("%lld", &arr[i]);
   }
+  qsort(arr, N, sizeof(long long int), myCompa);
 
-  scanf("%lld", &Q);
-  for (int i = 0; i < Q; i++) {
+  ll q;
+  scanf("%lld", &q);
+  for (int i = 0; i < q; i++) {
     ll l, r;
     scanf("%lld %lld", &l, &r);
-    ll total = 0;
-    for (int j = 0; j < N; j++) {
-      if (arr[j] >= l && arr[j] <= r) {
-        total += arr[j];
+
+    ll sum = 0;
+    if (l > r) {
+      printf("0\n");
+    } else {
+      if (l > arr[N - 1] || r < arr[0]) {
+        printf("0\n");
+      } else if (l < arr[0] && r > arr[N - 1]) {
+        for (ll j = 0; j < N; j++) {
+          sum += arr[j];
+        }
+        printf("%lld\n", sum);
+      } else if (l < arr[0] && (r >= arr[0] && r <= arr[N - 1])) {
+        ll right = rightmost(arr, N, r);
+        for (ll j = 0; j <= right; j++) {
+          sum += arr[j];
+        }
+        printf("%lld\n", sum);
+      } else if ((l >= arr[0] && l <= arr[N - 1]) && r > arr[N - 1]) {
+        ll left = leftmost(arr, N, l);
+        for (ll j = left; j < N; j++) {
+          sum += arr[j];
+        }
+        printf("%lld\n", sum);
+      } else if ((l >= arr[0] && l <= arr[N - 1]) &&
+                 (r >= arr[0] && r <= arr[N - 1])) {
+        ll left = leftmost(arr, N, l);
+        ll right = rightmost(arr, N, r);
+        for (ll j = left; j <= right; j++) {
+          sum += arr[j];
+        }
+        printf("%lld\n", sum);
       }
     }
-    printf("%lld\n", total);
   }
 }
